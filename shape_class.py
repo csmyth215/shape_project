9c6d2e701a3092f5c65651cfe5794085747f24f0
Chloe Smyth <chloe.smyth57@gmail.com>
2020-03-28 19:25:34 +0000

    Create an abstract class called Shape and several classes that inherit from it. Prompt the user to describe their shape. Allow the user to enter dimensions of two-dimensional shapes. Calculate perimeter and area of two-dimensional shapes.

--- shape_class.py
+++ shape_class.py
@@ -1,1 +1,33 @@
-# Create parent Shape class
+import math
+from abc import ABC, abstractmethod
+
+# Create parent Shape classes
+class TwoDimensionalShape(ABC):
+
+    @abstractmethod
+    def announce_shape(self):
+        ...
+
+    @abstractmethod
+    def get_area(self):
+        ...
+        
+    @abstractmethod
+    def get_perimeter(self):
+        ...
+
+
+
+class ThreeDimensionalShape(ABC):
+
+    @abstractmethod
+    def announce_shape(self):
+        ...
+
+    @abstractmethod
+    def get_surface_area(self):
+        ...
+
+    @abstractmethod
+    def get_volume(self):
+        ... 
