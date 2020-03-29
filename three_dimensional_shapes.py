b4538d2639f484f08cd8887b799003232aaa0d79
Chloe Smyth <chloe.smyth57@gmail.com>
2020-03-29 10:36:26 +0100

    Calculate shape surface area and volume
    Enable user input functions to collect metrics to be used in both
    2D and 3D shape calculations.

--- three_dimensional_shapes.py
+++ three_dimensional_shapes.py
@@ -9,33 +9,34 @@ class Sphere(ThreeDimensionalShape):
         return('sphere')
 
     def get_surface_area(self):
-        return(4 * math.pi * (r ** 2))    
+        return(4 * math.pi * (self.radius ** 2))    
 
     def get_volume(self):
         return(4/3 * math.pi * (self.radius ** 3))
 
 
-class SquarePrism(ThreeDimensionalShape):
-    def __init__(self, length, width, height):
+class QuadPrism(ThreeDimensionalShape):
+    def __init__(self, length, width, depth):
         self.length = length
         self.width = width
-        self.height = height
+        self.depth = depth
 
     def announce_shape(self):
         return('prism')
 
     def get_surface_area(self):
-        return((2 * (self.length * self.width)) + (2 * (self.length * self.height)) + (2 * (self.width * self.height)))   
+        # Only accurate for cubes or cuboids. Needs tailoring for prisms with other quadrilateral faces
+        return((2 * (self.length * self.width)) + (2 * (self.length * self.depth)) + (2 * (self.width * self.depth)))   
 
     def get_volume(self):
-        return(self.length * self.width * self.height)
+        return(self.length * self.width * self.depth)
 
 
 class TriangularPrism(ThreeDimensionalShape):
-    def __init__(self, b, l, ph=None, a=None, c=None):
+    def __init__(self, b, d, ph=None, a=None, c=None):
         self.b = b
         self.ph = ph
-        self.l = l
+        self.d = d
 
         if ph is not None:   
             self.a = math.sqrt(((0.5 * b) ** 2) + (ph ** 2))
@@ -53,27 +54,24 @@ class TriangularPrism(ThreeDimensionalShape):
         return(math.sqrt(self.halfp * (self.halfp - self.a) * (self.halfp - self.b) * (self.halfp - self.c)))
 
     def get_surface_area(self):
-        return((self.b * self.l) + (self.a * self.l) + (self.c * self.l) + (2 * self.get_heron_area()))
+        return((self.b * self.d) + (self.a * self.d) + (self.c * self.d) + (2 * self.get_heron_area()))
         
     def get_volume(self):
-        return((self.get_heron_area()) * self.l)
+        return((self.get_heron_area()) * self.d)
 
 
 class Cylinder(ThreeDimensionalShape):
-    def __init__(self, radius, length):
+    def __init__(self, radius, depth):
         self.radius = radius
-        self.length = length
+        self.depth = depth
         self.circumference = radius * 2 * math.pi
 
     def announce_shape(self):
         return('cylinder')
 
     def get_surface_area(self):
-        return((2 * math.pi * (self.radius ** 2)) + (self.circumference * self.length))
+        return((2 * math.pi * (self.radius ** 2)) + (self.circumference * self.depth))
 
     def get_volume(self):
-        return(math.pi * (self.radius ** 2) * self.length)
+        return(math.pi * (self.radius ** 2) * self.depth)
 
-my_3d_shape = TriangularPrism(8, 10, ph=3)
-print(my_3d_shape.get_heron_area())
-# print(f"The {my_3d_shape.announce_shape()} has a surface area of", "{0:.2f}".format(my_3d_shape.get_surface_area()), "square units and a volume of", "{0:.2f}".format(my_3d_shape.get_volume()), "cubic units.")
--- user_input.py
+++ user_input.py
@@ -1,59 +1,17 @@
+import sys
 from two_dimensional_shapes import *
 from three_dimensional_shapes import *
 
-def determine_dimension_track():
-    dimension_list = {2: 'Two dimensions', 3: 'Three dimensions'}
+# Currently lacking final input option to ask the user if they would like to run the program again.
 
-    print("Welcome to the shape metric calculator. \nHow many dimensions does your shape have?")
-    for x, y in dimension_list.items():
-        print(x,".", y)
-    validation_pending = True
-    while validation_pending == True:
-        dimension_count = (input())
-        try:
-            dimension_count = int(dimension_count)
-        except ValueError:
-            pass
-        if dimension_count in dimension_list.keys():
-            validation_pending = False 
-            print(f"\nWhich category does your {dimension_list[dimension_count][:-1].lower()}al shape best fit into?")
-            if dimension_count == 2:
-                select_two_dimensional_shape()
-            elif dimension_count == 3:
-                select_three_dimensional_shape()
-        else:
-            print("Please enter either '2' or '3'.")
+# Need to restructure code to separate user input from calculations, 
+# and group calculations common to several shapes.
 
-def select_two_dimensional_shape():
-    two_dimensional_inventory = {1: 'circle', 2: 'quadrilateral', 3: 'triangle', 4: 'other'}
-    for x, y in two_dimensional_inventory.items():
-        print(x,".", y)
-    validation_pending = True
-    while validation_pending == True:
-        shape_number = input()
-        try:
-            shape_number = int(shape_number)
-        except ValueError:
-            pass
-        if shape_number == 1:
-            validation_pending = False
-            perform_circle_calculations()
-        elif shape_number == 2:
-            validation_pending = False
-            perform_quad_calculations()
-        elif shape_number == 3:
-            validation_pending = False
-            determine_triangle_knowledge()
-        elif shape_number == 4:
-            validation_pending = False
-            print("Other shapes will be coming soon.")
-        else:
-            print("Please enter either '1', '2' '3' or '4'.")
-        
-      
+# Need to write automated tests to identify calculation or programming errors.
 
-def perform_circle_calculations():
-    print("Tell me the circle's radius: ")
+def get_radius(item):
+    item = str(item)
+    print(f"Tell me the radius of the {item}: ")
     validation_pending = True
     while validation_pending == True:
         rad = input()
@@ -73,12 +31,13 @@ def perform_circle_calculations():
             except ValueError:
                 print("Please enter a numerical value.")
 
-    my_circle = Circle(rad)
-    print("Your circle has radius", rad, "units, perimeter", '{0:.2f}'.format(my_circle.get_perimeter()), "units and area", '{0:.2f}'.format(my_circle.get_area()), "square units")
+    return(float(rad))
 
+def get_quad_properties(quad):
+    # requires wording alighment
 
-def perform_quad_calculations():
-    print("Tell me your shape's length and perpendicular height: ")
+    quad = str(quad)
+    print(f"Tell me the length and perpendicular height of {quad}: ")
     length_validation_pending = True
     perph_validation_pending = True
 
@@ -152,13 +111,14 @@ def perform_quad_calculations():
                 except ValueError:
                     print("Please enter a numerical value greater than 0 and less than or equal to 90.")
 
-        my_quad = Quadrilateral(leng, perph, angle)
-        print("Your shape has length", leng, "units, perpendicular height", perph, "units, perimeter", '{0:.2f}'.format(my_quad.get_perimeter()), "units and area", '{0:.2f}'.format(my_quad.get_area()), "square units")
+        properties = (leng, perph, angle)
+        properties_ints = []
+        for i in properties:
+            properties_ints.append(int(i))
+        return properties_ints
 
     else:
         print("The shape metric calculator doesn't work with trapezia just yet, sorry!")
-
-
 
 def determine_triangle_knowledge():
     print("What triangle metrics do you already know?")
@@ -172,10 +132,10 @@ def determine_triangle_knowledge():
             known_combo = int(known_combo)
             if known_combo == 1:
                 validation_pending = False
-                perform_triangle_calculations_with_two_metrics()
+                return get_bh()
             elif known_combo == 2:
                 validation_pending = False
-                perform_triangle_calculations_with_three_metrics()
+                return get_abc()
             elif known_combo == 3:
                 validation_pending = False
                 print("The calculator needs that minimum knowledge for now, sorry!")
@@ -185,8 +145,7 @@ def determine_triangle_knowledge():
             print("Please enter '1', '2', or '3'.")
             print("Debug!:", ex)
 
-
-def perform_triangle_calculations_with_two_metrics():
+def get_bh():
     print("Tell me the base and perpendicular height of your triangle: ")
     base_validation_pending = True
     perph_validation_pending = True
@@ -227,11 +186,15 @@ def perform_triangle_calculations_with_two_metrics():
             except ValueError:
                 print("Please enter a numerical value greater than 0.")   
 
-    my_tri = Triangle(base, ph=perph)
-    print("Your triangle has base", base, "units, perpendicular height", perph, "units, perimeter", '{0:.2f}'.format(my_tri.get_perimeter()), "units and area", '{0:.2f}'.format(my_tri.get_area()), "square units")
+        bh = []
+        base = float(base)
+        ph = float(perph)
+        bh.append(base)
+        bh.append(perph)
+    
+    return bh
 
-
-def perform_triangle_calculations_with_three_metrics():
+def get_abc():
     print("Tell me the lengths of the three sides of your triangle: ")
 
     abc_validation_pending = True
@@ -261,19 +224,219 @@ def perform_triangle_calculations_with_three_metrics():
                         print("Please enter numerical values greater than 0.")
                 except ValueError:
                     print("Please enter numerical values greater than 0.")
+        
+        abcfloats = []
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        abcfloats.append(a)
+        abcfloats.append(b)
+        abcfloats.append(c)
 
-        if count == 3:
-            abc_validation_pending = False
+        if a + b > c and b + c > a and a + c > b and count == 3:
+            abc_validation_pending = False 
+        else:
+            print("Your three lengths do not make a valid triangle.  Please try again.")
     
-    a = float(a)
-    b = float(b)
-    c = float(c)
-# Handle math domain error i.e. combination of lengths not possible to make triangle.
+    return abcfloats
 
-    my_tri = Triangle(b, a=a, c=c)
-    print("Your triangle has lengths", a, b, "and", c, "units, perimeter", '{0:.2f}'.format(my_tri.get_perimeter()), "units and area", '{0:.2f}'.format(my_tri.get_area()), "square units")
+def determine_prism_type():
+    print("What shape is the base of your prism?")
+    prisms = {1: "Quadrilateral", 2: "Triangle", 3: "Other"}
+    for x, y in prisms.items():
+        print(x,".", y)
+    validation_pending = True
+    while validation_pending == True:
+        prism = input()
+        try:
+            prism = int(prism)
+            if prism == 1:
+                validation_pending = False
+                this_prism_properties = (1, get_quad_properties("one prism face"))
+                return this_prism_properties
+            elif prism == 2:
+                validation_pending = False
+                print("Thinking of the triangular face of your prism...")
+                this_prism_properties = (2, determine_triangle_knowledge())
+                return this_prism_properties
+            elif prism == 3:
+                validation_pending = False
+                print("The calculator isn't built for prisms with so may size yet, sorry!")
+            else:
+                print("Please enter '1', '2', or '3'.")
+        except ValueError as ex:
+            print("Please enter '1', '2', or '3'.")
+            print("Debug!:", ex)
 
-def select_three_dimensional_shape():
-    print("3D is coming soon!")
+def determine_depth(shape):
+    print(f"Tell me the depth of your {shape}: ") 
+    validation_pending = True
+    while validation_pending == True:
+        depth = input("Depth: ")
+        try: 
+            depth = int(depth)
+            if depth > 0:
+                length_validation_pending = False
+            else:
+                print("Please enter a numerical value greater than 0.")  
+        except:
+            try:
+                depth = float(depth)
+                if depth > 0:
+                    length_validation_pending = False
+                else:
+                    print("Please enter a numerical value greater than 0.")
+            except ValueError:
+                print("Please enter a numerical value.")
+        
+        return(float(depth))
+
+def size_two_dimensional_shape():
+    two_dimensional_inventory = {1: 'Circle', 2: 'Quadrilateral', 3: 'Triangle', 4: 'Other'}
+    for x, y in two_dimensional_inventory.items():
+        print(x,".", y)
+    validation_pending = True
+    while validation_pending == True:
+        shape_number = input()
+        try:
+            shape_number = int(shape_number)
+        except ValueError:
+            pass
+        if shape_number == 1:
+            validation_pending = False
+            this_radius = get_radius("circle")
+            my_circle = Circle(this_radius)
+            print(f"Your circle has radius", this_radius, "units, perimeter", 
+            '{0:.2f}'.format(my_circle.get_perimeter()), "units and area", 
+            '{0:.2f}'.format(my_circle.get_area()), "square units")
+        
+        elif shape_number == 2:
+            validation_pending = False
+            this_quad = get_quad_properties("your quadrilateral")
+            my_quad = Quadrilateral(this_quad[0], this_quad[1], this_quad[2])
+            print("Your shape has length", this_quad[0], "units, 
+            perpendicular height", this_quad[1], "units, perimeter", 
+            '{0:.2f}'.format(my_quad.get_perimeter()), "units and area", 
+            '{0:.2f}'.format(my_quad.get_area()), "square units")
+        
+        elif shape_number == 3:
+            validation_pending = False
+            properties = determine_triangle_knowledge()
+            if properties is None:
+                print("Aborting program for now (insufficient knowledge).")
+                sys.exit()
+            if len(properties) == 3:
+                # abc known            
+                my_tri = Triangle(properties[1], a=properties[0], c=properties[2])
+                print("Your triangle has lengths", properties[0],",", properties[1], 
+                "and", properties[2], "units, perimeter", '{0:.2f}'.format(my_tri.get_perimeter()), 
+                "units and area", '{0:.2f}'.format(my_tri.get_area()), "square units")
+
+            elif len(properties) == 2:
+                # base and height known
+                my_tri = Triangle(properties[0], ph=properties[1])
+                print("Your triangle has base", properties[0], "units, perpendicular height", 
+                properties[1], "units, perimeter", '{0:.2f}'.format(my_tri.get_perimeter()), 
+                "units and area", '{0:.2f}'.format(my_tri.get_area()), "square units")
+
+            else:
+                print("Error encountered. Contact administrator.")
+
+        elif shape_number == 4:
+            validation_pending = False
+            print("Other shapes will be coming soon.")
+        else:
+            print("Please enter either '1', '2' '3' or '4'.")
+
+def size_three_dimensional_shape():
+    three_dimensional_inventory = {1: 'Sphere', 2: 'Prism', 3: 'Cylinder', 4: 'Other'}
+    for x, y in three_dimensional_inventory.items():
+        print(x,".", y)
+    validation_pending = True
+    while validation_pending == True:
+        shape_number = input()
+        try:
+            shape_number = int(shape_number)
+        except ValueError:
+            pass
+        if shape_number == 1:
+            validation_pending = False
+            this_radius = get_radius("sphere")
+            my_sphere = Sphere(this_radius)
+            print(f"Your sphere has radius", this_radius, 
+            ", surface area", '{0:.2f}'.format(my_sphere.get_surface_area()), 
+            "square units and volume", '{0:.2f}'.format(my_sphere.get_volume()), "cubic units.")
+        
+        elif shape_number == 2:
+            validation_pending = False
+            properties = determine_prism_type()
+            if properties[1] is None:
+                print("Aborting program for now (insufficient knowledge).")
+                sys.exit()    
+            d = determine_depth("prism")
+
+            if properties[0] == 1:
+                my_quad_prism = QuadPrism(properties[1][0], properties[1][1], d)
+                print(f"Your prism has surface area", 
+                '{0:.2f}'.format(my_quad_prism.get_surface_area()), 
+                "square units and volume", '{0:.2f}'.format(my_quad_prism.get_volume()), "cubic units.")
+      
+            elif properties[0] == 2:
+                if len(properties[1]) == 3:
+                    # abc known  
+                    my_tri_prism = TriangularPrism(properties[1][1], d, a=properties[1][0], c=properties[1][2])
+                    print("Your prism has surface area", 
+                    '{0:.2f}'.format(my_tri_prism.get_surface_area()), 
+                    "square units and volume", '{0:.2f}'.format(my_tri_prism.get_volume()), "cubic units.")
+      
+                elif len(properties[1]) == 2:
+                    # base and height known
+                    my_tri_prism = TriangularPrism(properties[1][0], d, ph=properties[1][1])
+                    print("Your prism has surface area", 
+                    '{0:.2f}'.format(my_tri_prism.get_surface_area()), 
+                    "square units and volume", 
+                    '{0:.2f}'.format(my_tri_prism.get_volume()), "cubic units.")
+                
+                else:
+                    print("Error encountered. Contact administrator.")
+      
+        elif shape_number == 3:
+            validation_pending = False
+            this_radius = get_radius("circular face")
+            d = determine_depth("cylinder")
+            my_cylinder = Cylinder(this_radius, d)
+            print(f"Your cylinder has radius", this_radius, 
+            ", depth", d, 
+            ", surface area", '{0:.2f}'.format(my_cylinder.get_surface_area()), 
+            "square units and volume", '{0:.2f}'.format(my_cylinder.get_volume()), "cubic units.")
+        
+        elif shape_number == 4:
+            validation_pending = False
+            print("Other shapes will be coming soon.")
+        else:
+            print("Please enter either '1', '2' '3' or '4'.")
+
+def determine_dimension_track():
+    dimension_list = {2: 'Two dimensions', 3: 'Three dimensions'}
+
+    print("Welcome to the shape metric calculator. \nHow many dimensions does your shape have?")
+    for x, y in dimension_list.items():
+        print(x,".", y)
+    validation_pending = True
+    while validation_pending == True:
+        dimension_count = (input())
+        try:
+            dimension_count = int(dimension_count)
+        except ValueError:
+            pass
+        if dimension_count in dimension_list.keys():
+            validation_pending = False 
+            print(f"\nWhich category does your {dimension_list[dimension_count][:-1].lower()}al shape best fit into?")
+            if dimension_count == 2:
+                size_two_dimensional_shape()
+            elif dimension_count == 3:
+                size_three_dimensional_shape()
+        else:
+            print("Please enter either '2' or '3'.")
 
 determine_dimension_track()
